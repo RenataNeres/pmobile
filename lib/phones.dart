4401db344c6cd6752f3517e97dd3d3216fef7a6f
Reh Neres <108026050+RenataNeres@users.noreply.github.com>
2022-10-24 13:42:41 -0300

    Update phones.dart

--- lib/phones.dart
+++ lib/phones.dart
@@ -1,33 +1,21 @@
-/*class Phones {
-  final String text;
-  final double number;
+class Phones {
+  late final String text;
+  late final double numero;
 
   Phones({
     required this.text,
-    required this.number,
+    required this.numero,
   });
-}
-
-Phones.fromJson(Map<String, dynamic> json) {
-    var texto = json['text'];
-    var numero = json['number'];
-  }*/
-
-class Phones {
-  String? text;
-  double? number;
-
-  Phones({this.text, this.number});
 
   Phones.fromJson(Map<String, dynamic> json) {
     text = json['text'];
-    number = json['number'];
+    numero = json['number'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['text'] = text;
-    data['number'] = number;
+    data['number'] = numero;
     return data;
   }
 }
