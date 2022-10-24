ce81583e5a422057aa0d6eb6dc527367677d5b2e
Reh Neres <108026050+RenataNeres@users.noreply.github.com>
2022-10-24 13:39:45 -0300

    Update bd_helper.dart

--- lib/bd_helper.dart
+++ lib/bd_helper.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/rendering.dart';
 import 'package:path/path.dart';
-//import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
   initDB() async {
     String databasesPath = await getDatabasesPath();
     String databaseName = join(databasesPath, 'pacote.db');
-    var db = await openDatabase(
+    Database db = await openDatabase(
       databaseName,
       version: 1,
       onCreate: onCreate,
@@ -22,45 +22,39 @@ class DBHelper {
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (1, 'Direitos Humanos', '100');";
+    "INSERT INTO package (text, number) VALUES (1, 'Direitos Humanos', '100');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (2, 'Centro de Velorização a Vida', '180');";
+    "INSERT INTO package (text, number) VALUES (2, 'Centro de Velorização a Vida', '180');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (3, 'Disque Denúncia', '181');";
+    "INSERT INTO package (text, number) VALUES (3, 'Disque Denúncia', '181');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (4, 'Polícia Militar', '190');";
+    "INSERT INTO package (text, number) VALUES (4, 'Polícia Militar', '190');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (5, 'Ambulância Pública (SAMU)', '192');";
+    "INSERT INTO package (text, number) VALUES (5, 'Ambulância Pública (SAMU)', '192');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (6, 'Corpo de Bombeiros', '193');";
+    "INSERT INTO package (text, number) VALUES (6, 'Corpo de Bombeiros', '193');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (7, 'Polícia Civil', '197');";
+    "INSERT INTO package (text, number) VALUES (7, 'Polícia Civil', '197');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (8, 'Polícia Rodoviária Estadual', '198');";
+    "INSERT INTO package (text, number) VALUES (8, 'Polícia Rodoviária Estadual', '198');";
     await db.execute(sql);
 
     sql =
-        "INSERT INTO package (text, number) VALUES (9, 'Defesa Civil', '199');";
+    "INSERT INTO package (text, number) VALUES (9, 'Defesa Civil', '199');";
     await db.execute(sql);
   }
-
-  openDatabase(String databaseName,
-      {required int version,
-      required Function(dynamic db, int version) onCreate}) {}
 }
-
-getDatabasesPath() {}
